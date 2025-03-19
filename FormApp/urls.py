db39014b82196fd28cb4559159ef1b4eb3a8cf3a
sabina tamang <syangtansabia8@gmail.com>
2025-03-19 20:38:44 +0545

    first commit

--- FormApp/urls.py
+++ FormApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import register ,log_in
 
 urlpatterns = [
-    path('', register, name='register'), 
-    path('login/',log_in,name='log_in'),
+    path('register/', register, name='register'), 
+    path('',log_in,name='log_in'),
 
 ]
